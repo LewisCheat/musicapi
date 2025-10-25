bbb42a48b2bd2586e2ac0fd525227fa0d1207106
LewisCheat <lewisvn1234@gmail.com>
2025-10-25 18:57:08 +0700

    Update music.js

--- music.js
+++ music.js
@@ -1,4 +1,4 @@
-  const remix = [
+  const playlist = [
     'https://files.catbox.moe/s0nyo2.mp4', /* Am Tham Ben Em Slex */
     'https://files.catbox.moe/0821zl.mp3', /* Khong Buong X Nang Co Mang Em Ve */
     'https://files.catbox.moe/idzhjg.mp3', /* Hong Kong 1 x Loi Tam Biet Chua Noi */
@@ -15,3 +15,4 @@ const vpop = [
     '',
     '',
 ]
+
